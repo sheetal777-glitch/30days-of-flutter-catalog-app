f03006f174d0d995393aa006c69591bb32787339
sheetal777-glitch <sheetalguragain@gmail.com>
2021-05-14 17:29:01 +0545

    day15 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -31,23 +31,50 @@ class _HomePageState extends State<HomePage> {
     setState(() {});
   }
 
-  @override 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: const Text("Catalog App"),
       ),
       drawer: MyDrawer(),
       body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: (CatalogModel.items.isNotEmpty)
-              ? ListView.builder(
+              ? GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 16,
+                      crossAxisCount: 2),
                   itemCount: CatalogModel.items.length,
                   itemBuilder: (BuildContext context, int index) {
-                    return ItemWidget(
-                      item: CatalogModel.items[index],
+                    final item = CatalogModel.items[index];
+                    return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                        child: Image.network(item.image!),
+                        header: Container(
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(color: Colors.deepPurple),
+                          child: Text(
+                            item.name!,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        footer: Container(
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(color: Colors.black),
+                          child: Text(
+                            item.price.toString(),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
                     );
-                  })
+                  },
+                )
               : Center(
                   child: CircularProgressIndicator(),
                 )),
