2d6649af63ae73f9e226cba5b400c85795fe00b8
sheetal777-glitch <sheetalguragain@gmail.com>
2021-05-14 16:32:31 +0545

    day13 completed

--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -9,9 +9,9 @@ class ItemWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        leading: Image.network(item.image),
-        title: Text(item.name),
-        subtitle: Text(item.desc),
+        leading: Image.network(item.image!),
+        title: Text(item.name!),
+        subtitle: Text(item.desc!),
         trailing: Text(
           "\$${item.price}",
           textScaleFactor: 1.5,
