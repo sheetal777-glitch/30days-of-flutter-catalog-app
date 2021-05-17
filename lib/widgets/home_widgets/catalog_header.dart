71bd6d584fa7f67fd8dcf66401f992fac4bb134e
sheetal777-glitch <sheetalguragain@gmail.com>
2021-05-17 15:27:18 +0545

    day20 completed

--- lib/widgets/home_widgets/catalog_header.dart
+++ lib/widgets/home_widgets/catalog_header.dart
@@ -2,14 +2,37 @@ import 'package:catalog_app/widgets/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-class CatalogHeader extends StatelessWidget {
+class CatalogHeader extends StatefulWidget {
+  @override
+  _CatalogHeaderState createState() => _CatalogHeaderState();
+}
+
+class _CatalogHeaderState extends State<CatalogHeader> {
+  bool switchValue = false;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        "Catalog App".text.xl5.bold.color(MyTheme.darkBluishColor).make(),
-        "Trending products".text.xl2.make(),
+        "Catalog App".text.xl5.bold.color(context.theme.accentColor).make(),
+        Row(
+          children: [
+            "Trending products"
+                .text
+                .color(context.theme.accentColor)
+                .xl2
+                .make(),
+            Switch(
+                value: switchValue,
+                onChanged: (val) {
+                  setState(() {
+                    val = switchValue;
+                  });
+                  print("switched");
+                }),
+          ],
+        ),
       ],
     );
   }
